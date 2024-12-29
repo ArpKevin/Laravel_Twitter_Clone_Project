607816aa968dfe153edb9fb650cd357e79e2b917
ArpKevin <arpaskevin11@gmail.com>
2024-12-29 01:40:36 +0100

    ep10 - form submission implemented

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,16 +9,6 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        // $idea = new Idea();
-        // $idea->content = "test";
-        // $idea->likes = 0;
-        // $idea->save();
-
-        $idea = new Idea([
-            'content' => 'hello ytb'
-        ]);
-        $idea->save();
-
         return view("dashboard", [
             'ideas'=> Idea::orderBy('created_at', 'desc')->get()
         ]);
---
+++ app/Http/Controllers/IdeaController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Idea;
+
+class IdeaController extends Controller
+{
+    public function store(){
+
+        // $idea = new Idea();
+        // $idea->content = "test";
+        // $idea->likes = 0;
+        // $idea->save();
+
+        Idea::create([
+            'content' => request('idea', null)
+        ]);
+
+        return to_route('dashboard');
+        // return redirect()->route('dashboard');
+    }
+}
