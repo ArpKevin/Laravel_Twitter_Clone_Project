91d41a3f2f67938ae3a6dfb5a08aff37eb3ad797
Kevin <arpaskevin11@gmail.com>
2025-01-07 12:36:30 +0100

    ep26 - profile page

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,30 @@
+@extends('shared.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-3">
+            @include('shared.left-sidebar')
+        </div>
+        <div class="col-6">
+            <div class="mt-3">
+                @include('shared.success-message')
+                @include('shared.user-card')
+            </div>
+            <hr>
+            @forelse ($ideas as $idea)
+                <div class="mt-3">
+                    @include('shared.idea-card')
+                </div>
+            @empty
+                <p class="text-center mt-4">No results found.</p>
+            @endforelse
+            <div class="mt-2">
+                {{ $ideas->withQueryString()->links() }}
+            </div>
+        </div>
+        <div class="col-3">
+            @include('shared.search-bar')
+            @include('shared.follow-box')
+        </div>
+    </div>
+@endsection
