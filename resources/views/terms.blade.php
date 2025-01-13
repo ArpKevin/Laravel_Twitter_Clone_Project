c9f0757691eedeecde3b621aa93831603e76ab61
Kevin <arpaskevin11@gmail.com>
2025-01-13 20:53:00 +0100

    ep30 - active menu links

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -2,9 +2,21 @@
 
 
 @section('content')
-
-<h1>Terms</h1>
-<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor veritatis atque vitae numquam accusamus neque, ea nobis. Iure maiores, quas vel dolorum pariatur architecto voluptates incidunt rerum fugit reiciendis eaque culpa natus recusandae illum reprehenderit, sint similique? Facilis ipsum esse eos quas qui aut officia tempora veniam animi rem maiores nam doloremque enim magni, vel id numquam odio cupiditate reprehenderit.</div>
-
-
+    <div class="row">
+        <div class="col-3">
+            @include('shared.left-sidebar')
+        </div>
+        <div class="col-6">
+            <h1>Terms</h1>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor veritatis atque vitae numquam accusamus
+                neque, ea nobis. Iure maiores, quas vel dolorum pariatur architecto voluptates incidunt rerum fugit
+                reiciendis eaque culpa natus recusandae illum reprehenderit, sint similique? Facilis ipsum esse eos quas qui
+                aut officia tempora veniam animi rem maiores nam doloremque enim magni, vel id numquam odio cupiditate
+                reprehenderit.</div>
+        </div>
+        <div class="col-3">
+            @include('shared.search-bar')
+            @include('shared.follow-box')
+        </div>
+    </div>
 @endsection
