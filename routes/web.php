4a6ad88d02a0f1ecde998d6f6728e4aefca55ea2
Kevin <arpaskevin11@gmail.com>
2025-04-20 23:57:07 +0200

    added PinController to web.php left out by accident

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PinController;
 
 /*
 |--------------------------------------------------------------------------
