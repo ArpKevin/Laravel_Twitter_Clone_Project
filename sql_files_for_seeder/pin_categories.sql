09cb02ea5dc3689ce0aa689928f15ede5e727f04
Kevin <arpaskevin11@gmail.com>
2025-04-25 23:16:35 +0200

    changed the sql data to english

--- sql_files_for_seeder/pin_categories.sql
+++ sql_files_for_seeder/pin_categories.sql
@@ -1,7 +1,13 @@
 INSERT INTO pin_categories (category_name, category_color) VALUES
-('Történelmi események', '#8B0000'),
-('Neves személyek', '#1E90FF'),
-('Emléktáblák', '#FFD700'),
-('Egyéb alkotások', '#32CD32'),
-('Nem köztéri alkotások', '#8A2BE2'),
-('Lebontott alkotások', '#A9A9A9');
+
+("Historical events", "#8B0000"),
+
+("Famous people", "#1E90FF"),
+
+("Memorial plaques", "#FFD700"),
+
+("Other works", "#32CD32"),
+
+("Non-public works", "#8A2BE2"),
+
+("Demolished works", "#A9A9A9");
