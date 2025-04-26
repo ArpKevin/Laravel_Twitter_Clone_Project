f2223aae8395738997e8cb3870346554a9113a31
Kevin <arpaskevin11@gmail.com>
2025-04-26 17:25:09 +0200

    changed the sql queries to include the created_at and the updated_at for the manual insertion of pin_categories and pins

--- sql_files_for_seeder/pin_categories.sql
+++ sql_files_for_seeder/pin_categories.sql
@@ -1,13 +1,13 @@
-INSERT INTO pin_categories (category_name, category_color) VALUES
+INSERT INTO pin_categories (category_name, category_color, created_at, updated_at) VALUES
 
-("Historical events", "#8B0000"),
+("Historical events", "#8B0000", NOW(), NOW()),
 
-("Famous people", "#1E90FF"),
+("Famous people", "#1E90FF", NOW(), NOW()),
 
-("Memorial plaques", "#FFD700"),
+("Memorial plaques", "#FFD700", NOW(), NOW()),
 
-("Other works", "#32CD32"),
+("Other works", "#32CD32", NOW(), NOW()),
 
-("Non-public works", "#8A2BE2"),
+("Non-public works", "#8A2BE2", NOW(), NOW()),
 
-("Demolished works", "#A9A9A9");
+("Demolished works", "#A9A9A9", NOW(), NOW());
